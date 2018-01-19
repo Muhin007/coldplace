fda26da80b99559eb33502cff3cfcf83a6fa41aa
Muhin007 <9226343456@mail.ru>
2018-01-19 15:23:42 +0500

    Создал файл "Scanner.java" с функцией ввода информации с консоли.

---
+++ maven/src/main/java/com/github/muhin007/coldplace/Scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Scanner {
+    public static void main(String[] args) {
+        System.out.print("Введите город: ");
+        Scanner sc = new Scanner(System.in);
+        String sity = scanner.nextInt();
+        System.out.print("Вы указали город: " + sity);
+    }
+    }

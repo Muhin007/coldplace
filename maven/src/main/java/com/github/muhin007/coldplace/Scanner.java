6be76db5b23a5c8180d87e550bf4f2fbd2d08807
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-21 23:11:09 +0500

    Update Scanner.java

--- maven/src/main/java/com/github/muhin007/coldplace/Scanner.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Scanner.java
@@ -5,7 +5,7 @@ public class Scanner {
     public static void main(String[] args) {
         System.out.print("Введите город: ");
         Scanner sc = new Scanner(System.in);
-        String sity = scanner.nextInt();
-        System.out.print("Вы указали город: " + sity);
+        String city = scanner.nextInt();
+        System.out.print("Вы указали город: " + city);
     }
     }

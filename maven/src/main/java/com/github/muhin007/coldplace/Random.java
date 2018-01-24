26d436b917fec6ed6f6806a24a8bf7a75fc05fa5
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-24 22:08:45 +0500

    Внесение изменений в Класс и цикл
    Внес изменения в классRandom. Переписал название класса с заглавной буквы, как это принято в Java. Убрал инкремент из цикла.

--- maven/src/main/java/com/github/muhin007/coldplace/Random.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Random.java
@@ -2,10 +2,10 @@ package com.github.muhin007.coldplace;
 
 import java.util.Random;
 
-class random{
+class Random{
     public static void main(String[] args){
         Random r = new Random();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 1) {
             int q = r.nextInt(101) - 50;
             System.out.println(q);
         }

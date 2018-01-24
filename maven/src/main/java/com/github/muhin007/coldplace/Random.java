e7c55f65a04c4250d41b31094b2a3ea66ae4b04d
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-24 15:15:37 +0500

    Update Random.java
    Исправление кода с целью получения на выдаче только 1 (одного) случайного числа

--- maven/src/main/java/com/github/muhin007/coldplace/Random.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Random.java
@@ -3,7 +3,7 @@ import java.util.Random;
 class random{
     public static void main(String[] args){
         Random r = new Random();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1; i++) {
             int q = r.nextInt(101) - 50;
             System.out.println(q);
         }

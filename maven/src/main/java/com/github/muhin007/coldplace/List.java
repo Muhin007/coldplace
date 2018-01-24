9118f101b8aaab336188ed13cd73027abdf5dc25
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-24 13:19:01 +0500

    Список городов
    Создан список городов для получения данных по температуре.

---
+++ maven/src/main/java/com/github/muhin007/coldplace/List.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+pablic class List{
+
+  public static void main(String[] args) {
+  List<String> list = new ArrayList<>();
+  list.add("Москва");
+  list.add("Санкт-Петербург");
+  list.add("Челябинск");
+  list.add("Новосибирск");
+  list.add("Магадан");
+  list.add("Владивосток");
+}
+}

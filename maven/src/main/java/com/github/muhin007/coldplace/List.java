e6d0a2a76959c4e61fb97395ed466c79dbc8bf48
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-24 13:35:39 +0500

    Список городов
    Импортирован дополнительный класс

--- maven/src/main/java/com/github/muhin007/coldplace/List.java
+++ maven/src/main/java/com/github/muhin007/coldplace/List.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.List;
 
-pablic class List{
+class list{
 
-  public static void main(String[] args) {
-  List<String> list = new ArrayList<>();
-  list.add("Москва");
-  list.add("Санкт-Петербург");
-  list.add("Челябинск");
-  list.add("Новосибирск");
-  list.add("Магадан");
-  list.add("Владивосток");
-}
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Москва");
+        list.add("Санкт-Петербург");
+        list.add("Челябинск");
+        list.add("Новосибирск");
+        list.add("Магадан");
+        list.add("Владивосток");
+    }
 }

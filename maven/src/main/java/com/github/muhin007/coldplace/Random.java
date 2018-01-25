87a409ff26ecf3fbb6cebe440569ddc09783714b
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-25 11:36:43 +0500

    Убрал цикл
    Убрал цикл для упрощения кода

--- maven/src/main/java/com/github/muhin007/coldplace/Random.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Random.java
@@ -5,9 +5,8 @@ import java.util.Random;
 class Random{
     public static void main(String[] args){
         Random r = new Random();
-        for (int i = 0; i < 1) {
-            int q = r.nextInt(101) - 50;
-            System.out.println(q);
-        }
+        int i = 0;
+        int q = r.nextInt(101) - 50;
+        System.out.println(q);
     }
 }

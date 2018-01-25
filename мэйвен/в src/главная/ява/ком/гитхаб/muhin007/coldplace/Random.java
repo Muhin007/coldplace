bc00852695deaaef7e890c8d7b0cfd4a207408b9
Muhin007 <35358700+Muhin007@users.noreply.github.com>
2018-01-25 12:16:46 +0500

    Убрал неиспользующуюся строку
    После исключения цикла осталась переменная, которая не участвует в программе. Убрал эту переменную.

--- мэйвен/в src/главная/ява/ком/гитхаб/muhin007/coldplace/Random.java
+++ мэйвен/в src/главная/ява/ком/гитхаб/muhin007/coldplace/Random.java
@@ -5,7 +5,6 @@ import java.util.Random;
 class Random{
     public static void main(String[] args){
         Random r = new Random();
-        int i = 0;
         int q = r.nextInt(101) - 50;
         System.out.println(q);
     }

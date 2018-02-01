f43df824f6126d946e28686d38f8531b3cdcd35e
Muhin007 <9226343456@mail.ru>
2018-02-01 22:14:05 +0500

    Добавлено задание для следующего занятия

--- maven/src/main/java/com/github/muhin007/coldplace/City.java
+++ maven/src/main/java/com/github/muhin007/coldplace/City.java
@@ -11,5 +11,49 @@ class City {
         this.minTemperature = minTemperature;
         this.maxTemperature = maxTemperature;
     }
+    /*
+    City city = list.get(0);
+    double d = (Math.abs(city.maxTemperature - city.minTemperature) * r.nextDouble()) + city.minTemperature;
+    int i = (int) d;
+     */
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (o == this) {
+//            return true;
+//        }
+//        if (!(o instanceof City)) {
+//            return false;
+//        }
+//        City otherCity = (City) o;
+//        if (this.name.equals(otherCity.name) &&
+//                this.minTemperature == otherCity.minTemperature &&
+//                this.maxTemperature == otherCity.maxTemperature) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        City city = (City) o;
+        return name.equals(city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + minTemperature;
+        result = 31 * result + maxTemperature;
+        return result;
+    }
+
+    public int calculateRandomTemperature() {
+        return 0;
+    }
 }
--- maven/src/main/java/com/github/muhin007/coldplace/Coldplace.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Coldplace.java
@@ -10,12 +10,12 @@ public class Coldplace {
     public static void main(String[] args) {
         System.out.println("Программа показывает температуру в запрашиваемом городе");
 
-        List<City> List = new ArrayList<>();
-        List.add(new City("Москва", -20, -5));
-        List.add(new City("Санкт-Петербург", -15, 0));
-        List.add(new City("Новосибирск", -25, -15));
-        List.add(new City("Магадан", -40, -20));
-        List.add(new City("Владивосток", -20, -10));
+        List<City> list = new ArrayList<City>();
+        list.add(new City("Москва", -20, -5));
+        list.add(new City("Санкт-Петербург", -15, 0));
+        list.add(new City("Новосибирск", -25, -15));
+        list.add(new City("Магадан", -40, -20));
+        list.add(new City("Владивосток", -20, -10));
 
         Random r = new Random();
         int m1 = r.nextInt();//доработать выдачу случайного числа из диапазона температур для каждого города
@@ -25,8 +25,16 @@ public class Coldplace {
         System.out.print("Введите название города ");
         String name = sc.nextLine();
 
-        if (List.contains(name)) {
-            System.out.println("Сейчас в " + name + " " + m1);
+        City foundedCity = null;
+        for (City city : list) {
+            if (city.name.equals(name)) {
+                foundedCity = city;
+                break;
+            }
+        }
+
+        if (foundedCity != null) {
+            System.out.println("Сейчас в " + name + " " + foundedCity.calculateRandomTemperature());
         } else {
             System.out.println("Сейчас в " + name + " " + x);
 

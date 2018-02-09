6de04b65bfea767d5f7ae0f50c2362d8f3cca238
Muhin007 <9226343456@mail.ru>
2018-02-09 12:02:02 +0500

    Добавлено запись/чтение списка в/из файла. Добавлены коментарии к блокам кода.

--- maven/src/main/java/com/github/muhin007/coldplace/Coldplace.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Coldplace.java
@@ -1,8 +1,6 @@
 package com.github.muhin007.coldplace;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,9 +10,9 @@ import java.util.logging.Logger;
 
 public class Coldplace {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         System.out.println("Программа показывает температуру в запрашиваемом городе");
-
+//Создали список городов
         List<City> list = new ArrayList<>();
         list.add(new City("Москва", -20, -5));
         list.add(new City("Челябинск", -25, -10));
@@ -22,52 +20,68 @@ public class Coldplace {
         list.add(new City("Новосибирск", -25, -15));
         list.add(new City("Магадан", -40, -20));
         list.add(new City("Владивосток", -20, -10));
-
+//Записали список городов в файл
         Writer writer = null;
         try {
-            writer = new FileWriter("ListCity.txt");
+            writer = new FileWriter("Citylist.txt");
             for (City list : list) {
-                writer.write(list);
+                writer.write();
                 writer.write(System.getProperty("line.separator"));
             }
             writer.flush();
         } catch (Exception e) {
-            Logger.getLogger(Coldplace.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(TestClass.class.getName()).log(Level.SEVERE, null, e);
         } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException ignored) {
-                }
+            if (writer != null) try {
+                writer.close();
+            } catch (IOException ignored) {
             }
         }
-
-        Random r = new Random();
-        int x = r.nextInt(101) - 50;
-
+        //Прочитали список городов из файла
+        try {
+            File file = new File("Citylist.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//Приняли название города с консоли
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите название города ");
         String name = sc.nextLine();
-
+//Сравниваем введенный город со списком и получаем случайную температуру
+        City city = list.get(0);
+        double d = (Math.abs(city.getMaxTemperature() - city.getMinTemperature()) * r.nextDouble()) + city.getMinTemperature();
+        int i;
+        switch ( i = (int) d ) {
+        }
         City foundedCity = null;
-        for (City city : list) {
+        for (City list : list) {
             if (city.name.equals(name)) {
                 foundedCity = city;
                 break;
             }
         }
-
+//Ввели случайный выбор температур если введенного города нет в списке
+        Random r = new Random();
+        int x = r.nextInt(101) - 50;
+//выводим на экран название города и случайное значение температуры
         if (foundedCity != null) {
-            System.out.println("Сейчас в " + name + " " + foundedCity.calculateRandomTemperature());
+            System.out.println("Сейчас в " + name + " " + i);
         } else {
             System.out.println("Сейчас в " + name + " " + x);
         }
-        
+//Записываем в файл название города и случайное значение температуры
         FileWriter fw = new FileWriter("C:\\Dev\\Project\\coldplace\\maven\\src\\main\\" +
                 "java\\com\\github\\muhin007\\coldplace\\City.txt");
 
-            fw.write(name + " " + foundedCity.calculateRandomTemperature());
+        fw.write(name + " " + i);
 
-            fw.close();
+        fw.close();
     }
 }

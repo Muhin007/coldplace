d5367eeda59316f824fadafc8b9acc10700cc7e8
Muhin007 <9226343456@mail.ru>
2018-02-19 14:26:29 +0500

    Issue COLD-5 Реализована запись данных из списка "City" в текстовый файл "Citylist".

--- maven/src/main/java/com/github/muhin007/coldplace/Main.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Программа показывает температуру в запрашиваемом городе");
 
-//Создали список городов
         List<City> list = new ArrayList<>();
         list.add(new City("Москва", -20, -5));
         list.add(new City("Челябинск", -25, -10));
@@ -19,7 +18,7 @@ public class Main {
         list.add(new City("Новосибирск", -25, -15));
         list.add(new City("Магадан", -40, -20));
         list.add(new City("Владивосток", -20, -10));
-//Записали список городов в файл
+
         FileWriter writer = new FileWriter("Citylist.txt");
         for (City news : list) {
             String name = news.getName();
@@ -30,29 +29,10 @@ public class Main {
         }
         writer.close();
 
-        //Прочитали список городов из файла
-        try {
-
-            FileReader fr = new FileReader("Citylist.txt");
-            BufferedReader reader = new BufferedReader(fr);
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//Приняли название города с консоли
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите название города ");
         String name = sc.nextLine();
-//Сравниваем введенный город со списком и получаем случайную температуру
-        City city = list.get(0);
-        double d = (Math.abs(city.getMaxTemperature() - city.getMinTemperature()) * r.nextDouble()) + city.getMinTemperature();
-        int i = (int) d;
-        {
-        }
+
         City foundedCity = null;
         for (City city : list) {
             if (city.name.equals(name)) {
@@ -60,16 +40,23 @@ public class Main {
                 break;
             }
         }
-//Ввели случайный выбор температур если введенного города нет в списке
+
         Random r = new Random();
-        int x = r.nextInt(101) - 50;
-//выводим на экран название города и случайное значение температуры
-        if (foundedCity != null) {
-            System.out.println("Сейчас в " + name + " " + i);
-        } else {
-            System.out.println("Сейчас в " + name + " " + x);
+        for (City city :list){
+            int minTemperature = city.getMinTemperature();
+            int maxTemperature = city.getMaxTemperature();
+            double d = (Math.abs((maxTemperature - minTemperature) * r.nextDouble()) + minTemperature);
+            int i = (int) d;
+            int x = r.nextInt(101) - 50;
+
+
+            if (foundedCity != null) {
+                System.out.println("Сейчас в " + name + " " + i);
+            } else {
+                System.out.println("Сейчас в " + name + " " + x);
+
+            }
 
         }
     }
-}
 }

383f3fe62eae6383b7cce49b3b85ffc1e149dd39
Mikhail Materukhin <9226343456@mail.ru>
2018-03-01 21:38:00 +0600

    COLD-8 Сделан тест City.calculateRandomTemperature

--- maven/src/test/java/com/github/muhin007/coldplace/CityTest.java
+++ maven/src/test/java/com/github/muhin007/coldplace/CityTest.java
@@ -1,22 +1,16 @@
 package com.github.muhin007.coldplace;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Random;
 
 public class CityTest {
 
     @Test
     public void calculateRandomTemperature() {
-        Random r = new Random();
-        int minTemperature = -30;
-        int maxTemperature = -10;
-        double d = (Math.abs((maxTemperature - minTemperature) * r.nextDouble()) + minTemperature);
-        int actual = (int) d;
-        if (actual >= -30 && actual <= -10) {
-            System.out.print("Тест пройден");}
-         else {
-            System.out.print("Тест не пройден");
+        City city = new City("testName", -30, -10);
+        for (int i = 0; i < 10; i++) {
+            int t = city.calculateRandomTemperature();
+            Assert.assertTrue(t >= -30 && t < -10);
         }
     }
 }

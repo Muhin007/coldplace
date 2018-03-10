dd4eab8c67acd8ed4a45ee166e4a90f2acb9e098
Mikhail Materukhin <9226343456@mail.ru>
2018-03-11 00:38:25 +0600

    COLD-10 Исправлен поиск аргумента и добавлена стоп-команда

--- maven/src/main/java/com/github/muhin007/coldplace/Main.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Main.java
@@ -3,35 +3,31 @@ package com.github.muhin007.coldplace;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        if ( args[0].equals("-h") )
-            System.out.println( "-h, --help - помощь в использовании ключей командной строки; " +
+
+        if ( args[0].equals("-h") || args[0].equals("--help"))
+            System.out.println("-h, --help - помощь в использовании ключей командной строки; " +
                     "-с, --city - температуру в указанном городе; --city-list - список названий городов;" +
                     " запуск программы без ключа покажет случайную темературу в случайном городе");
-        if ( args[1].equals("--help") )
-            System.out.println( "-h, --help - помощь в использовании ключей командной строки;" +
-                    "-с, --city - температуру в указанном городе; --city-list - список названий городов;" +
-                    " запуск программы без ключа покажет случайную темературу в случайном городе");
-        if ( args[2].equals("-c") )
-            System.out.println("Введите название города");
-        if ( args[3].equals("--city") )
-            System.out.println( "Введите название города" );
-        if ( args[4].equals("--city-list") )
+        System.exit(0);
+        if ( args[0].equals("-c") || args[0].equals("--city") )
+            System.out.print(args[1]);
+        System.exit(0);
+        if ( args[0].equals("--city-list") )
             System.out.println( "Список городов" );
-        else System.out.println( "Случайную температуру");
+        System.exit(0);
+        if (args[0] == null)
+            System.out.println( "Случайную температуру");
 
         System.out.println("Программа показывает температуру в запрашиваемом городе");
 
         List<City> cities = new ArrayList<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("CityRead.txt"))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader("C://Dev//Project//coldplace//CityRead.txt"))) {
             String line = reader.readLine();
             while (line != null) {
                 String[] stringsArray = line.split(",", 3);

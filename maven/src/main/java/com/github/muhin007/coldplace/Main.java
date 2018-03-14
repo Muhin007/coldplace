4db5713efa4ff5dd88e911dfdc5132bfe5b72b88
Mikhail Materukhin <9226343456@mail.ru>
2018-03-14 23:16:56 +0600

    COLD-10 Дописан ключ -f и --data-file-path дающий возможность изменять файл для чтения.

--- maven/src/main/java/com/github/muhin007/coldplace/Main.java
+++ maven/src/main/java/com/github/muhin007/coldplace/Main.java
@@ -16,10 +16,11 @@ public class Main {
         }
         if (args[0].equals("-h") || args[0].equals("--help")) {
             System.out.println("Программа Coldplace показывает температуру в запрашиваемом городе");
-            System.out.println("-h, --help - помощь в использовании ключей командной строки; " +
-                    "-с, --city <Название города> - покажет вам температуру в указанном городе; " +
-                    "--city-list - покажет вам список названий всех доступных городов; " +
-                    "p.s. запуск программы без ключа покажет случайную температуру");
+            System.out.println("-h, --help - помощь в использовании ключей командной строки;" +
+                    "-с, --city <Название города> - покажет вам температуру в указанном городе;" +
+                    "--city-list - покажет вам список названий всех доступных городов;" +
+                    "-f и --data-file-path указанные после -с, --city, --city-list позволят указать путь к другому файлу сдругим списком городов" +
+                    " p.s. запуск программы без ключа покажет случайную температуру");
             return;
         }
         if (args[0].equals("-c") || args[0].equals("--city")) {
@@ -28,23 +29,36 @@ public class Main {
             List<City> cities = new ArrayList<>();
 
             try {
-                File file = new File(args[2]);
-                BufferedReader reader = new BufferedReader
-                        (new InputStreamReader(new FileInputStream(file), "UTF8"));
-                String line = reader.readLine();
-                while (line != null) {
-                    String[] stringsArray = line.split(",", 3);
-                    String name = stringsArray[0];
-                    int minTemperature = Integer.parseInt(stringsArray[1].trim());
-                    int maxTemperature = Integer.parseInt(stringsArray[2].trim());
-                    cities.add(new City(name, minTemperature, maxTemperature));
-
-                    line = reader.readLine();
+                if (args[1].equals("-f") || args[1].equals("--data-file-path")) {
+                    File file = new File(args[2]);
+                    BufferedReader reader = new BufferedReader
+                            (new InputStreamReader(new FileInputStream(file), "UTF8"));
+                    String line = reader.readLine();
+                    while (line != null) {
+                        String[] stringsArray = line.split(",", 3);
+                        String name = stringsArray[0];
+                        int minTemperature = Integer.parseInt(stringsArray[1].trim());
+                        int maxTemperature = Integer.parseInt(stringsArray[2].trim());
+                        cities.add(new City(name, minTemperature, maxTemperature));
+                        line = reader.readLine();
+                    }
+                } else {
+                    File file = new File("CityRead.txt");
+                    BufferedReader reader = new BufferedReader
+                            (new InputStreamReader(new FileInputStream(file), "UTF8"));
+                    String line = reader.readLine();
+                    while (line != null) {
+                        String[] stringsArray = line.split(",", 3);
+                        String name = stringsArray[0];
+                        int minTemperature = Integer.parseInt(stringsArray[1].trim());
+                        int maxTemperature = Integer.parseInt(stringsArray[2].trim());
+                        cities.add(new City(name, minTemperature, maxTemperature));
+                        line = reader.readLine();
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace(); //TODO log to file, not show to user
             }
-
             City foundedCity = null;
             for (City city : cities) {
                 if (args[1].equalsIgnoreCase(city.getName())) {
@@ -60,16 +74,30 @@ public class Main {
         }
         if (args[0].equals("--city-list")) {
             try {
-                System.out.println("Города доступные для просмотра:");
-                File file = new File(args[1]);
-                BufferedReader reader = new BufferedReader
-                        (new InputStreamReader(new FileInputStream(file), "UTF8"));
-                String line = reader.readLine();
-                while (line != null) {
-                    String[] stringsArray = line.split(",", 3);
-                    String name = stringsArray[0];
-                    System.out.println(name);
-                    line = reader.readLine();
+                if (args[1].equals("-f") || args[1].equals("--data-file-path")) {
+                    System.out.println("Города доступные для просмотра:");
+                    File file = new File(args[2]);
+                    BufferedReader reader = new BufferedReader
+                            (new InputStreamReader(new FileInputStream(file), "UTF8"));
+                    String line = reader.readLine();
+                    while (line != null) {
+                        String[] stringsArray = line.split(",", 3);
+                        String name = stringsArray[0];
+                        System.out.println(name);
+                        line = reader.readLine();
+                    }
+                } else {
+                    System.out.println("Города доступные для просмотра:");
+                    File file = new File("CityRead.txt");
+                    BufferedReader reader = new BufferedReader
+                            (new InputStreamReader(new FileInputStream(file), "UTF8"));
+                    String line = reader.readLine();
+                    while (line != null) {
+                        String[] stringsArray = line.split(",", 3);
+                        String name = stringsArray[0];
+                        System.out.println(name);
+                        line = reader.readLine();
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();//TODO log to file, not show to user
